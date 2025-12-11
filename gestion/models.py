d470630971dcb03a3239a77fe2e1c044be06d53b
Idelia Gomez <ideliagomez2@gmail.com>
2025-12-11 16:11:11 -0600

    Remove unused import of Venta model

--- gestion/models.py
+++ gestion/models.py
@@ -5,7 +5,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db.models import Sum
 from decimal import Decimal
-from .models import Venta
 
 class Cliente(models.Model):
     id_cliente = models.AutoField(primary_key=True)
