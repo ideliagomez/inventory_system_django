efe934607354e805cc0714010dc9fa7f022e5219
ideliagomez <ideliagomez2@gmail.com>
2025-12-12 19:10:40 -0600

    fix: working demo restrictions middleware

--- inventory_app/settings.py
+++ inventory_app/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'inventory_app.middleware.DemoRestrictionsMiddleware',
 ]
 
 ROOT_URLCONF = "inventory_app.urls"
